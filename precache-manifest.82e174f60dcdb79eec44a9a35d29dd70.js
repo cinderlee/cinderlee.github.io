6112bb095756b1f454cec893fdbb51a3e3f94b40
Cindy Lee <leecindy405@gmail.com>
2020-07-14 16:28:57 -0400

    Updates

---
+++ precache-manifest.82e174f60dcdb79eec44a9a35d29dd70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035d07338870e428845ddc35bebea453",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d0b14eb937fe1a48fbb",
+    "url": "/static/css/main.8d05184e.chunk.css"
+  },
+  {
+    "revision": "0e010341850a9c6caa5d",
+    "url": "/static/js/2.15263b8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15263b8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d0b14eb937fe1a48fbb",
+    "url": "/static/js/main.acbec2a7.chunk.js"
+  },
+  {
+    "revision": "d8c959d448685c90fdb0",
+    "url": "/static/js/runtime-main.9123d8d1.js"
+  }
+]);
